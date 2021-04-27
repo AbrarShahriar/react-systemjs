e31c974356304a70d0d11263b52a0781969f635e
AbrarShahriar <AbrarShahriar321@gmail.com>
2021-04-28 00:09:40 +0600

    first commit

---
+++ package.js
@@ -0,0 +1,32 @@
+SystemJS.config({
+  baseURL:'',
+  defaultExtension: true,
+  packages: {
+    ".": {
+      main: './app.js',
+      defaultExtension: 'js'
+    }
+  },
+  meta: {
+    '*.js': {
+      'babelOptions': {
+        react: true
+      }
+    },
+    '*.css': {
+      loader: 'css'
+    }
+  },
+  map: {
+    'plugin-babel': 'https://unpkg.com/systemjs-plugin-babel@latest/plugin-babel.js',
+    'systemjs-babel-build': 'https://unpkg.com/systemjs-plugin-babel@latest/systemjs-babel-browser.js',
+    'react': 'https://unpkg.com/react@17.0.1/umd/react.development.js',
+    'react-dom': 'https://unpkg.com/react-dom@17.0.1/umd/react-dom.development.js',
+    'css': 'https://unpkg.com/systemjs-plugin-css@0.1.37/css.js',
+    'logo.svg': '../src/logo.svg'
+  },
+  transpiler: 'plugin-babel'
+});
+
+SystemJS.import('../src/index.js')
+  .catch(console.error.bind(console));
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <img className="App-logo" src="images/logo.svg" />
+          <h3>Edit src/App.js and save to reload</h3>
+          <a className="App-link" href="https://reactjs.org/docs/getting-started.html">Learn React</a>
+      </header>
+    </div>
+  )
+}
+
+export default App
