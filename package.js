febb67b17459be66bda3fc569aae245b3a1db85f
AbrarShahriar <AbrarShahriar321@gmail.com>
2021-04-28 13:12:06 +0600

    performance improvement

--- package.js
+++ package.js
@@ -18,10 +18,10 @@ SystemJS.config({
     }
   },
   map: {
-    'plugin-babel': 'https://unpkg.com/systemjs-plugin-babel@latest/plugin-babel.js',
-    'systemjs-babel-build': 'https://unpkg.com/systemjs-plugin-babel@latest/systemjs-babel-browser.js',
-    'react': 'https://unpkg.com/react@17.0.1/umd/react.development.js',
-    'react-dom': 'https://unpkg.com/react-dom@17.0.1/umd/react-dom.development.js',
+    'plugin-babel': '../node_modules/babel/plugin-babel.js',
+    'systemjs-babel-build': '../node_modules/systemjs/systemjs-babel-browser.js',
+    'react': '../node_modules/react/react.production.min.js',
+    'react-dom': '../node_modules/react-dom/react-dom.production.min.js',
     'css': 'https://unpkg.com/systemjs-plugin-css@0.1.37/css.js',
   },
   transpiler: 'plugin-babel'
